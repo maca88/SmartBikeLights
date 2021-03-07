172cb9ede07513258aa5af29b0325c05ccbb11f1
maca88 <bostjan.markezic@siol.net>
2021-03-07 22:17:51 +0100

    Add battery filter note

--- Source/light-configurator/src/filters/BatteryFilter.js
+++ Source/light-configurator/src/filters/BatteryFilter.js
@@ -1,4 +1,5 @@
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import AppSelect from '../inputs/AppSelect';
 import AppSlider from '../inputs/AppSlider';
 import { operatorList, batteryStateList, getBatteryOperator, getBatteryValue } from '../constants';
@@ -17,6 +18,12 @@ export default observer(({ filter }) => {
 
   return (
   <Grid container spacing={3}>
+    <Grid item xs={12} sm={12}>
+      <Typography>
+      NOTE: The battery state is not defined in percentage but rather as a number from one to five, where number one is "New" and five is "Bad". 
+      Each number represents a percentage range (e.g. 50%-75%), where the range for each number may differ from light to light.
+      </Typography>
+    </Grid>
     <Grid item xs={12} sm={4}>
       <AppSelect required items={operatorList} label="Operator" setter={setOperator} value={getBatteryOperator(filter.operator)} /> 
     </Grid>
