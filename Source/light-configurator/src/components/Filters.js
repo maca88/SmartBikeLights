966ea8d7b2117bce9b2cc9b008df58932cff1c48
maca88 <bostjan.markezic@siol.net>
2022-06-24 23:06:21 +0200

    Removed unused variable

--- Source/light-configurator/src/components/Filters.js
+++ Source/light-configurator/src/components/Filters.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import Accordion from './Accordion';
 import AccordionSummary from './AccordionSummary';
